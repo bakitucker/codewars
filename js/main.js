227bbc7f04f2a0775bfa09c724b8a234a0e76035
bakitucker <bakitucker8@gmail.com>
2023-03-17 23:52:39 -0400

    added another codewar

--- js/main.js
+++ js/main.js
@@ -5,4 +5,31 @@ function greet(){
     const helloWorld = "hello world!"
     return helloWorld
   }
+
   
+
+
+
+  // write a function that takes an array of words
+
+// concatenate words from the array
+// put them into a sentence and return the sentence
+
+// add spaces between each word
+// no space at the beginning or the end of the sentence
+function smash (words) {
+  let sentence = ''
+  if(words.length === 0)return ''
+    
+  if(words.length === 1) return words[0]
+  
+  for(let i = 0; i< words.length; i++){
+    sentence = sentence + words[i] + ' '
+  }
+  
+   return sentence.trim()
+};
+
+
+
+
