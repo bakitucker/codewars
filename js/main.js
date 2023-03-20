bfbf04d188d457a85bbda12cc3f1cce60f19f853
bakitucker <bakitucker8@gmail.com>
2023-03-19 22:56:11 -0400

    I added two more codewars to it

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ function greet(){
 
 
 
+
+
+
+
   // write a function that takes an array of words
 
 // concatenate words from the array
@@ -39,3 +43,38 @@ function smash (words) {
 function multiply(a, b){
   product = a * b
   return product}
+
+
+
+
+
+
+  // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+  function simpleMultiplication(n) {
+    if(n % 2 === 0 ){
+       return n * 8
+      }
+      else{
+      return n * 9;
+      }    
+  }
+
+
+
+
+
+
+
+//   Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+const DNAtoRNA = dna => dna.replace(/T/g, 'U');
